47287bc7ccaec4b08f26bd2c9fcf1b1ed1f9046a
AbdallahSay <luka3122003@gmail.com>
2025-06-24 01:03:13 +0300

    Update Program.cs logic

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-          
+            #region Comments & Regions
+            // sigle line comment
+            //int x = 10;
+            //int y = 20;
+            /*
+             * Multiple Line Comment  
+             * int x = 12; 
+             * int y = 13;
+             *
+             */
+
+
+            ///XML Comment
+            /// <summary>
+            /// Sum Two Integers 
+            /// </summary>
+            /// <param name="x">  first Number </param>
+            /// <param name="y"> Second Number </param>
+            /// <returns> Sum of x + y  </returns>
+            //static int sum(int x, int y)
+            //{
+
+            //    return x + y;
+
+            //}
+            #endregion
+
         }
     }
 }
